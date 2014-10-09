1e1e59308d9d6b7c470100a6e3c91df556449eb6
Darwin Sri <sampathsrianuradha@ymail.com>
2014-10-09 20:00:09 +0530

    added PORT

--- includes/connection.php
+++ includes/connection.php
@@ -11,4 +11,28 @@
     if(!$db_select){
         die("Database selection failed: " . mysql_error());
     }
+
+class dbConnect{
+
+	private $host= DB_SERVER ;
+	private $db = DB_NAME;
+	private $port = DB_PORT;
+	private $user = DB_USER;
+	private $pass = DB_PASS;
+
+private static $instance = NULL;
+	private function __construct() {
+	  /*** maybe set the db name here later ***/
+	}
+
+public static function getInstance() {
+			if (!self::$instance)
+				{ 
+			
+				self::$instance = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));;
+				self::$instance-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+				}
+		return self::$instance;
+	}
 ?>
