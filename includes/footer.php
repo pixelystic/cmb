299a0528f3376f5286a2614575e29494fef9cb4c
Darwin Sri <sampathsrianuradha@ymail.com>
2014-11-04 23:24:23 +0530

    Test

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p> Colombo Today 2014</p>
+                    <p> Colombo Today 2015</p>
                 </div>
             </div>
         </footer>
